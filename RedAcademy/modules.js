1a709c3f686463255b67caa1a75b9a6165ced7e6
Patrick <phunanon@users.noreply.github.com>
2018-03-31 11:14:38 +0100

    Add files via upload

---
+++ RedAcademy/modules.js
@@ -0,0 +1,135 @@
+
+let _modules = [
+
+    { topic: "Welcome",    level: 0,  title: "Welcome to Red Academy", content: `
+<p>This online course serves as an easy-to-digest, complete-beginner's resource to anti-capitalism.</p>
+<p>I have tried to write it in simple English.</p>
+<p>Read each section in order, and by the end, you will have a basic understanding of capitalism, Marxism, and other anti-capitalist analysis & movements.</p>
+    ` },
+
+    { topic: "Marxism",       level: 0,  title: "Marxism: An Introduction", content: `
+<p><b>Marxism</b> is an analysis of modern society, from the writing of Karl Marx and Friedrich Engels. Some basic conclusions of Marxism are:</p>
+<ul>
+    <li>World society must go through socialism before it reaches communism.</li>
+    <li>World society is in conflict between the workers and the employers.</li>
+    <li>World economy, in socialism, must be democratically controlled by all workers.</li>
+    <li>World society must go through popular revolution, not voting, to reach socialism.</li>
+</ul>
+<p>You will learn why these are Marxism's conclusions throughout this course.</p>
+<p>Marxism also shows why things have value in money, and how trade works.</p>
+` },
+
+    { topic: "Marxism",       level: 0,  title: "Capital", content: `
+<p>The most important focus of Marxism is <b>capital</b>, and who owns it. Capital is anything with value for making products. These include the <u>materials, tools, buildings, and land</u>, used by workers.</p>
+<p>For example, a taxi driver uses their car to transport passengers around a city. The taxi car is capital, as it is used to earn money.</p>
+<p>Capital is sometimes called the <b>means of production,</b> as it helps to make products for sale.</p>
+<p>Slaves are also capital, as no worker uses them like a tool, but they still create value.</p>
+` },
+
+    { topic: "Marxism",       level: 1,  title: "Personal/Private Property", content: `
+<p>Capital is owned as property by either a person or business under capitalism. However, Marxism instead sees property into two different types:</p>
+<ul>
+    <li><b>Private property</b>: owned to be used by others (workers), and maybe the owner too, to earn money.</li>
+    <li><b>Personal property</b>: owned only to be used by the owner, or others for free when the owner says it is okay.</li>
+</ul>
+<p>For example, if you own a bed, and only you sleep on it (or other people sleep on it for free), it is <u>personal</u> property. If you own a bed, and other people pay to sleep on it (like a hotel), it is <u>private</u> property.</p>
+<p>For example, if you own 1 car, and only you use it, and maybe drive other people for free (or for some fuel money), it is <u>personal</u> property. If you own 10 cars, you do not use all the cars by yourself, and rent them to other people, making the cars <u>private</u> property.</p>
+` },
+
+    { topic: "Marxism",       level: 1,  title: "Economic Classes", content: `
+<p>Remembering private/personal property, Marxism puts each person of the world into 3 classes:</p>
+<ul>
+    <li>One of the <b>Proletariat</b>: does not own the property they work with.</li>
+    <li>One of the <b>Petite-Bourgeoisie</b>: does own all or some of the property they work with.</li>
+    <li>One of the <b>Bourgeoisie</b>: owns property, but does not use it themselves.</li>
+</ul>
+<p>One of the <u>Proletariat</u> (a worker) could be a clothes maker. They are paid for their work, but they do not own the machines, materials, or land they use.</p>
+<p>One of the <u>Petite-Bourgeoisie</u> could be a small shop owner. They buy and sell products, and own or rent the building, but they also work there, or a few people work together.</p>
+<p>One of the <u>Bourgeoisie</u> (a capitalist) could be a landlord. They own a building and land, and rent it out. Usually they do not have to work (other than directing others).</p>
+<p>There are also two types of Bourgeoisie: industrial and financial.</p>
+<hr>
+<p>How to pronounce: proletariat (pro-lit-airy-at), petite (peh-teet), bourgeoisie (bor-jwaz-ee).</p>
+<p>As an adjective: bourgeois (bor-jwaz). E.g. <i>bourgeois culture</i>.</p>` },
+
+    { topic: "Marxism",       level: 1,  title: "Profit", content: `
+<p><b>Profit</b> is what a capitalist uses to get money for themselves. The two different types of capitalist get profit differently.</p>
+<p>The <b>industrial capitalist</b> owns capital, and their workers give labour, and so the capitalist owns the finished product. The worker will be paid a wage, as decided when they start work, but it will never be the full value of the product or service.</p>
+<p>Below shows how an <u>industrial capitalist</u> uses their capital and workers to get money:</p>
+<table>
+    <tr>
+        <th>Production</th>
+        <th>Sale</th>
+        <th>Payment</th>
+        <th>Spending</th>
+    </tr><tr>
+        <td>Labour<br>+<br>Capital<br><big>&rarr;</big> Product</td>
+        <td class="vr">Market<br>value</td>
+        <td class="vr"><table><tr><td>Wage<br>40%<br><big>&darr;</big></td><td class="vr">Surplus<br>60%<br><big>&#8627;</big></td></tr></table><p>Worker</p></td>
+        <td class="vr">Buy new capital<br>Pay taxes<br>Pay loans & rent<br>----<br>The rest is profit<br>for the capitalist</td>
+    </tr>
+</table>
+<hr>
+<p>In Marxism, a sold product is usually called a <i>commodity.</i>` },
+
+    { topic: "Marxism",       level: 0,  title: "Class Conflict", content: `
+<p>Marxism concludes that society is constantly in conflict, between the Proletariat and Bourgeois classes.</p>
+<p>A worker wants to increase their wage, but a capitalist wants to lower it.</p>` },
+
+    { topic: "Marxism",       level: 0,  title: "Reformism", content: `` },
+
+    { topic: "Revolution",    level: 0,  title: "Reformism", content: `` },
+
+    { topic: "Revolution",    level: 0,  title: "National Question", content: `` },
+
+    { topic: "Civil Rights",  level: 0,  title: "", content: `` },
+
+    { topic: "Feminism",      level: 0,  title: "", content: `` },
+
+    { topic: "Veganism",      level: 0,  title: "", content: `` }
+];
+
+
+
+/*
+Marxism roadmap
+- It's a scientific analysis of a particular part of society
+- It can reduce complex economic problems into their widest scope
+- It's based in materialism, not idealism
+- It's served me well
+
+- The means of production
+   - Patrick's parable of the absentee sewing machines
+   - Personal/private property
+   - Our relation to the means of production (prole, petite, bourgie)
+      - "Lower/Middle/Upper" idealist distraction
+   - Prole labour as a commodity
+- Surplus value
+   - Its extraction
+   - Its disposal
+      - Highlight global/national inequality
+   - Its dictation (shareholding)
+   - Proles' mathematical starvation
+      - Liquidation crisis
+      - Credit & mortgage relief (w/cont. automation)
+- The pursuit of profit
+   - Planned obsolescence, ecology, waste & destruction, copyright
+   - Reduction of wages, welfare
+      - Unemployed pool
+   - Automation of jobs
+      - Surplus crisis
+   - Collaboration chaos (monopoly vs inefficient)
+- Solution: seize the means
+   - Unionising
+   - "Leninism, Trotskyism, Maoism"
+   - Reform: split shareholding, worker coop; tribalism
+   - Revolt: sans absentee property, workers' state
+   - Problems: material revolution, reaction, mercantilism
+- History: Burkina Faso
+   - Against foreign 'aid'
+   - Cash crop to edible
+   - Vaccine, women, literacy, desertification
+   - CDR's
+   - Unions, 'free' press
+- Observe: no idealism, no 'human nature' shit
+- Actual communism
+*/

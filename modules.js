76883d280598f7fda88c27aa7ab29ce166e796e7
Patrick Bowen <phun@protonmail.com>
2018-04-02 05:23:15 +0100

    Restructure repo; update RA content/CSS

--- modules.js
+++ modules.js
@@ -2,13 +2,13 @@
 let _modules = [
 
     { topic: "Welcome",    level: 0,  title: "Welcome to Red Academy", content: `
-<p>This online course serves as an easy-to-digest, complete-beginner's resource to anti-capitalism.</p>
+<p>This online course is an easy-to-digest, complete-beginner's resource to anti-capitalism.</p>
 <p>I have tried to write it in simple English.</p>
 <p>Read each section in order, and by the end, you will have a basic understanding of capitalism, Marxism, and other anti-capitalist analysis & movements.</p>
     ` },
 
     { topic: "Marxism",       level: 0,  title: "Marxism: An Introduction", content: `
-<p><b>Marxism</b> is an analysis of modern society, from the writing of Karl Marx and Friedrich Engels. Some basic conclusions of Marxism are:</p>
+<p><b>Marxism</b> is an analysis of modern society, from the writings of Karl Marx and Friedrich Engels. Some basic conclusions of Marxism are:</p>
 <ul>
     <li>World society must go through socialism before it reaches communism.</li>
     <li>World society is in conflict between the workers and the employers.</li>
@@ -21,13 +21,14 @@ let _modules = [
 
     { topic: "Marxism",       level: 0,  title: "Capital", content: `
 <p>The most important focus of Marxism is <b>capital</b>, and who owns it. Capital is anything with value for making products. These include the <u>materials, tools, buildings, and land</u>, used by workers.</p>
-<p>For example, a taxi driver uses their car to transport passengers around a city. The taxi car is capital, as it is used to earn money.</p>
+<p>For example, a taxi driver uses their car to drive passengers around a city. The taxi car is capital, as it is used to earn money.</p>
 <p>Capital is sometimes called the <b>means of production,</b> as it helps to make products for sale.</p>
 <p>Slaves are also capital, as no worker uses them like a tool, but they still create value.</p>
+<p><b>Money</b> is what a bank or government gives to trade different products easier. When someone has money, it is like a ticket for already existing capital. Sometimes, a bank or government will pretend they have capital when they give you money, hoping they have the capital later.</p>
 ` },
 
     { topic: "Marxism",       level: 1,  title: "Personal/Private Property", content: `
-<p>Capital is owned as property by either a person or business under capitalism. However, Marxism instead sees property into two different types:</p>
+<p>Capital is owned as property by a person or business in capitalism. However, Marxism instead sees property as two different types:</p>
 <ul>
     <li><b>Private property</b>: owned to be used by others (workers), and maybe the owner too, to earn money.</li>
     <li><b>Personal property</b>: owned only to be used by the owner, or others for free when the owner says it is okay.</li>
@@ -43,37 +44,74 @@ let _modules = [
     <li>One of the <b>Petite-Bourgeoisie</b>: does own all or some of the property they work with.</li>
     <li>One of the <b>Bourgeoisie</b>: owns property, but does not use it themselves.</li>
 </ul>
-<p>One of the <u>Proletariat</u> (a worker) could be a clothes maker. They are paid for their work, but they do not own the machines, materials, or land they use.</p>
+<p>One of the <u>Proletariat</u> (a worker) could be a clothes maker in a factory. They are paid for their work, but they do not own the machines, materials, or land they use.</p>
 <p>One of the <u>Petite-Bourgeoisie</u> could be a small shop owner. They buy and sell products, and own or rent the building, but they also work there, or a few people work together.</p>
 <p>One of the <u>Bourgeoisie</u> (a capitalist) could be a landlord. They own a building and land, and rent it out. Usually they do not have to work (other than directing others).</p>
-<p>There are also two types of Bourgeoisie: industrial and financial.</p>
+<p>There are two types of Bourgeoisie: industrial and financial.</p>
 <hr>
 <p>How to pronounce: proletariat (pro-lit-airy-at), petite (peh-teet), bourgeoisie (bor-jwaz-ee).</p>
 <p>As an adjective: bourgeois (bor-jwaz). E.g. <i>bourgeois culture</i>.</p>` },
 
-    { topic: "Marxism",       level: 1,  title: "Profit", content: `
-<p><b>Profit</b> is what a capitalist uses to get money for themselves. The two different types of capitalist get profit differently.</p>
+    { topic: "Marxism",       level: 1,  title: "Profit: Industrial", content: `
+<p><b>Profit</b> is money or products that a capitalist gets for themselves. The two different types of capitalist get profit differently.</p>
 <p>The <b>industrial capitalist</b> owns capital, and their workers give labour, and so the capitalist owns the finished product. The worker will be paid a wage, as decided when they start work, but it will never be the full value of the product or service.</p>
 <p>Below shows how an <u>industrial capitalist</u> uses their capital and workers to get money:</p>
 <table>
-    <tr>
-        <th>Production</th>
-        <th>Sale</th>
-        <th>Payment</th>
-        <th>Spending</th>
-    </tr><tr>
-        <td>Labour<br>+<br>Capital<br><big>&rarr;</big> Product</td>
-        <td class="vr">Market<br>value</td>
-        <td class="vr"><table><tr><td>Wage<br>40%<br><big>&darr;</big></td><td class="vr">Surplus<br>60%<br><big>&#8627;</big></td></tr></table><p>Worker</p></td>
-        <td class="vr">Buy new capital<br>Pay taxes<br>Pay loans & rent<br>----<br>The rest is profit<br>for the capitalist</td>
-    </tr>
+    <tr><th>1. Production</th><td>Labour + Capital <big>&rarr;</big> Product</td></tr>
+    <tr><th>2. Sale</th><td>Product sold at market value</td></tr>
+    <tr><th>3. Payment</th><td>Of market value:<br>30%: wage <big>&rarr;</big> worker<br>70%: surplus <big>&rarr;</big> capitalist</td></tr>
+    <tr><td><b>4. Spending</b><br>by capitalist</td><td>Buy new capital; Pay taxes, loans & rent<br>Pay managers, dividends<br>----<br>The rest is profit for the capitalist</td></tr>
 </table>
+<p>From this, we see that after the product is made, it is sold, and the value made from that sale is split. Some goes to wage, and the rest is called "surplus."</p>
+<p>Surplus is used to buy more capital, such as materials or other services.</p>
+<p>In 2017, the richest 1% of the world took 82% of new value made by workers (<a href="https://www.oxfam.org/en/pressroom/pressreleases/2018-01-22/richest-1-percent-bagged-82-percent-wealth-created-last-year">ref</a>). This shows surplus is very high.</p>
+<p>Marxism calls this whole process <b>exploitation</b>.</p>
 <hr>
-<p>In Marxism, a sold product is usually called a <i>commodity.</i>` },
+<p>In Marxism, a sold product is usually called a <i>commodity</i>.` },
+
+    { topic: "Marxism",       level: 1,  title: "Profit: Financial", content: `
+<p>A <b>financial capitalist</b> gets surplus in a different way. They own a lot of capital, such as housing, land, heavy machines, and shares in business.</p>
+<p>The first capital was taken with exploitation. When people (including industrial capitalists) do not have capital themselves, they will borrow from the financial capitalist. The capitalist is paid with <b>interest</b>, so they get more capital back than they gave.</p>
+<p>A person can be a financial capitalist <i>and</i> an industrial capitalist.</p>
+` },
+
+    { topic: "Marxism",       level: 1,  title: "Financial Crisis", content: `
+<p>The way an industrial capitalist gets surplus, with exploitation, causes financial crisis, like in 2008.</p>
+<p>When a worker is paid only some of the value they made, they then cannot buy back everything they produced.</p>
+<p>For example, if every person worked picking fruit - $20 of fruit picked per hour - but they were paid only $10 per hour, they could only buy half of what they produced.</p>
+<p>Capitalists do not fix this (by exploiting less) because they all want to exploit a big surplus for themselves. Also, they usually spend fast enough to avoid this problem.</p>
+<p>It is also slightly fixed by giving loans, mortgages, and credit to workers so they can still buy without money.</p>
+<p>However, it can go wrong: if surplus is invested in long-term capital, such as housing, the workers do not have the money to buy from the market. This causes a crash in prices.</p>
+` },
 
     { topic: "Marxism",       level: 0,  title: "Class Conflict", content: `
 <p>Marxism concludes that society is constantly in conflict, between the Proletariat and Bourgeois classes.</p>
 <p>A worker wants to increase their wage, but a capitalist wants to lower it.</p>` },
+
+    { topic: "Marxism",       level: 1,  title: "Shareholding", content: `
+<p></p>
+` },
+
+    { topic: "Marxism",       level: 1,  title: "Waste", content: `
+<p></p>
+` },
+
+    { topic: "Marxism",       level: 1,  title: "Automation", content: `
+<p></p>
+` },
+
+    { topic: "Marxism",       level: 1,  title: "Separation", content: `
+<p></p>
+//Loss of collaboration
+` },
+
+    { topic: "Marxism",       level: 1,  title: "Bourgeois State", content: `
+<p></p>
+` },
+
+    { topic: "Marxism",       level: 1,  title: "Fascism", content: `
+<p></p>
+` },
 
     { topic: "Marxism",       level: 0,  title: "Reformism", content: `` },
 
@@ -118,6 +156,7 @@ Marxism roadmap
    - Automation of jobs
       - Surplus crisis
    - Collaboration chaos (monopoly vs inefficient)
+   - Bourgeois state
 - Solution: seize the means
    - Unionising
    - "Leninism, Trotskyism, Maoism"
